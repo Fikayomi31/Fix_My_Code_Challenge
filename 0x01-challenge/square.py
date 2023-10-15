38fe38f9b8fdf00a0dd86158590019dd09dcb5e8
Fikayomi31 <fikayoogundijo@gmail.com>
2023-10-15 19:42:20 +0000

    my square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,10 +7,10 @@ class square():
     width = 0
     height = 0
 
-    def __init__(self, width, height):
+    def __init__(self, *args, **kwargs):
         """initiazation of class"""
-        self.width = width
-        self.height = height
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
